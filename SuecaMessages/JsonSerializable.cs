6254630d5b6da7380f9ff95790f83a19910eaaf8
PipzCorreiaz <filipacorreia@tecnico.ulisboa.pt>
2015-07-23 09:39:23 +0100

    Initial commit

---
+++ SuecaMessages/JsonSerializable.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace SuecaInterface
+{
+    public abstract class JsonSerializable
+    {
+        public string SerializeToJson()
+        {
+            var textWriter = new StringWriter();
+            var serializer = new JsonSerializer();
+            serializer.Serialize(textWriter, this);
+            return textWriter.ToString();
+        }
+
+        public static T DeserializeFromJson<T>(string serialized)
+        {
+            try
+            {
+                var textReader = new StringReader(serialized);
+                var serializer = new JsonSerializer();
+                return (T)serializer.Deserialize(textReader, typeof(T));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Failed to deserialize from '" + typeof(T) + "': " + e.Message);
+            }
+
+            return default(T);
+        }
+    }
+}
---
+++ SuecaMessages/SuecaMessages.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Thalamus;
+
+namespace SuecaMessages
+{
+    public interface IStartEvents : Thalamus.IPerception
+    {
+        void Init(int id, int trump, int firstPlayer, int[] initialCards);
+    }
+
+    public interface ICardsEvents : Thalamus.IPerception
+    {
+        void PlayedCard(int playerId, int card);
+        void Play();
+    }
+}
