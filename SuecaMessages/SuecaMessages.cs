7f251fb5d1ef3267cf3edeeaba730c9c19a2861f
pipzcorreiaz <filipaincorreia@gmail.com>
2015-08-31 11:29:47 +0100

    Add session events, shuffle, cut and deal

--- SuecaMessages/SuecaMessages.cs
+++ SuecaMessages/SuecaMessages.cs
@@ -4,8 +4,13 @@ namespace SuecaMessages
 {
     public interface ISuecaPerceptions : IPerception
     {
-        void GameStart(int id, int teamId, string trump, string[] cards);
+        void SessionStart(int numGames);
+        void GameStart(int gameId, int playerId, int teamId, string trump, string[] cards);
         void GameEnd(int team0Score, int team1Score);
+        void SessionEnd(int team0Score, int team1Score);
+        void Shuffle(int playerId);
+        void Cut(int playerId);
+        void Deal(int playerId);
         void NextPlayer(int id);
         void Play(int id, string card);
     }
