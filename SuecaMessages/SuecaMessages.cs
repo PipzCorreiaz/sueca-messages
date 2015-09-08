9578bd07463ca4b49dad23d8e9be9d9ce56f6530
PipzCorreiaz <filipacorreia@tecnico.ulisboa.pt>
2015-09-08 13:40:46 +0100

    Add expectation and desirabilities events

--- SuecaMessages/SuecaMessages.cs
+++ SuecaMessages/SuecaMessages.cs
@@ -23,6 +23,7 @@ namespace SuecaMessages
     public interface IIAActions : IAction
     {
         void Decision(string card, string followingInfo);
-        void ExpectedScores(int team0Score, int team1Score);
+        void Expectation(string successProbability, string failureProbability);
+        void MoveDesirabilities(string desirability, string desirabilityForOther);
     }
 }
