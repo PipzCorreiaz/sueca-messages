040b745dc80e6ff962c0b916ef2557a0cd2821b3
Filipa Correia <filipacorreia@tecnico.ulisboa.pt>
2016-03-24 16:44:52 +0000

    Add trump card messages

--- SuecaMessages/SuecaMessages.cs
+++ SuecaMessages/SuecaMessages.cs
@@ -5,12 +5,13 @@ namespace SuecaMessages
     public interface ISuecaPerceptions : IPerception
     {
         void SessionStart(int numGames);
-        void GameStart(int gameId, int playerId, int teamId, string trump, string[] cards);
+        void GameStart(int gameId, int playerId, int teamId, string trumpCard, int trumpCardPlayer, string[] cards);
         void GameEnd(int team0Score, int team1Score);
         void SessionEnd(int team0Score, int team1Score);
         void Shuffle(int playerId);
         void Cut(int playerId);
         void Deal(int playerId);
+        void TrumpCard(string trumpCard, int trumpCardPlayer);
         void ReceiveRobotCards();
         void TrickEnd(int winnerId, int trickPoints);
         void NextPlayer(int id);
@@ -30,12 +31,13 @@ namespace SuecaMessages
         void MoveExpectations(int playerId, string desirability, string desirabilityForOther, string successProbability, string failureProbability);
 
         void ForwardSessionStart(int numGames);
-        void ForwardGameStart(int gameId, int playerId, int teamId, string trump, string[] cards);
+        void ForwardGameStart(int gameId, int playerId, int teamId, string trumpCard, int trumpCardPlayer, string[] cards);
         void ForwardGameEnd(int team0Score, int team1Score);
         void ForwardSessionEnd(int team0Score, int team1Score);
         void ForwardShuffle(int playerId);
         void ForwardCut(int playerId);
         void ForwardDeal(int playerId);
+        void ForwardTrumpCard(string trumpCard, int trumpCardPlayer);
         void ForwardReceiveRobotCards();
         void ForwardTrickEnd(int winnerId, int trickPoints);
         void ForwardNextPlayer(int id);
