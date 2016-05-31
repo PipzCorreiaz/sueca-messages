13e09ad6ab3d4fa65479e37a20b3cb3a0e0c6572
Filipa Correia <filipacorreia@tecnico.ulisboa.pt>
2016-05-31 19:43:51 +0100

    Add many robots synchronization

--- SuecaMessages/SuecaMessages.cs
+++ SuecaMessages/SuecaMessages.cs
@@ -4,20 +4,20 @@ namespace SuecaMessages
 {
     public interface ISuecaPerceptions : IPerception
     {
-        void SessionStart(int numGames, int[] agentsIds);
-        void GameStart(int gameId, int playerId, int teamId, string trumpCard, int trumpCardPlayer, string[] cards);
-        void GameEnd(int team0Score, int team1Score);
-        void SessionEnd(int team0Score, int team1Score);
-        void Shuffle(int playerId);
-        void Cut(int playerId);
-        void Deal(int playerId);
-        void TrumpCard(string trumpCard, int trumpCardPlayer);
-        void ReceiveRobotCards();
-        void TrickEnd(int winnerId, int trickPoints);
-        void NextPlayer(int id);
-        void Play(int id, string card);
-        void Renounce(int playerId);
-        void ResetTrick();
+        void SessionStart(int numGames, int[] agentsIds, int talkingRobot);
+        void GameStart(int gameId, int playerId, int teamId, string trumpCard, int trumpCardPlayer, string[] cards, int talkingRobot);
+        void GameEnd(int team0Score, int team1Score, int talkingRobot);
+        void SessionEnd(int team0Score, int team1Score, int talkingRobot);
+        void Shuffle(int playerId, int talkingRobot);
+        void Cut(int playerId, int talkingRobot);
+        void Deal(int playerId, int talkingRobot);
+        void TrumpCard(string trumpCard, int trumpCardPlayer, int talkingRobot);
+        void ReceiveRobotCards(int playerId, int talkingRobot);
+        void TrickEnd(int winnerId, int trickPoints, int talkingRobot);
+        void NextPlayer(int id, int talkingRobot);
+        void Play(int id, string card, int talkingRobot);
+        void Renounce(int playerId, int talkingRobot);
+        void ResetTrick(int talkingRobot);
     }
 
     public interface ISuecaActions : IAction
@@ -25,23 +25,29 @@ namespace SuecaMessages
         void Play(int id, string card);
     }
 
+    public interface IRobotPerceptions : IAction
+    {
+        void StartedUtterance(int playerId, string category, string subcategory);
+        void FinishedUtterance(int playerId);
+    }
+
     public interface IIAActions : IAction
     {
         void Decision(string card, string rank, string suit, string followingInfo);
         void MoveExpectations(int playerId, string desirability, string desirabilityForOther, string successProbability, string failureProbability);
 
-        void ForwardSessionStart(int numGames);
-        void ForwardGameStart(int gameId, int playerId, int teamId, string trumpCard, int trumpCardPlayer, string[] cards);
-        void ForwardGameEnd(int team0Score, int team1Score);
-        void ForwardSessionEnd(int team0Score, int team1Score);
-        void ForwardShuffle(int playerId);
-        void ForwardCut(int playerId);
-        void ForwardDeal(int playerId);
-        void ForwardTrumpCard(string trumpCard, int trumpCardPlayer);
-        void ForwardReceiveRobotCards();
-        void ForwardTrickEnd(int winnerId, int trickPoints);
-        void ForwardNextPlayer(int id);
-        void ForwardRenounce(int playerId);
-        void ForwardResetTrick();
+        void ForwardSessionStart(int numGames, int talkingRobot);
+        void ForwardGameStart(int gameId, int playerId, int teamId, string trumpCard, int trumpCardPlayer, string[] cards, int talkingRobot);
+        void ForwardGameEnd(int team0Score, int team1Score, int talkingRobot);
+        void ForwardSessionEnd(int team0Score, int team1Score, int talkingRobot);
+        void ForwardShuffle(int playerId, int talkingRobot);
+        void ForwardCut(int playerId, int talkingRobot);
+        void ForwardDeal(int playerId, int talkingRobot);
+        void ForwardTrumpCard(string trumpCard, int trumpCardPlayer, int talkingRobot);
+        void ForwardReceiveRobotCards(int playerId, int talkingRobot);
+        void ForwardTrickEnd(int winnerId, int trickPoints, int talkingRobot);
+        void ForwardNextPlayer(int id, int talkingRobot);
+        void ForwardRenounce(int playerId, int talkingRobot);
+        void ForwardResetTrick(int talkingRobot);
     }
 }
