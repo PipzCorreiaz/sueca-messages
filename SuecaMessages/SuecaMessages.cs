5e212661223ef1f0c4e79a45ffed8cd6eb9745a2
Filipa Correia <filipacorreia@tecnico.ulisboa.pt>
2016-07-01 08:57:05 +0100

    Add sessionId to sessionStart and sessionEnd

--- SuecaMessages/SuecaMessages.cs
+++ SuecaMessages/SuecaMessages.cs
@@ -4,10 +4,10 @@ namespace SuecaMessages
 {
     public interface ISuecaPerceptions : IPerception
     {
-        void SessionStart(int numGames, int[] agentsIds);
+        void SessionStart(int sessionId, int numGames, int[] agentsIds);
         void GameStart(int gameId, int playerId, int teamId, string trumpCard, int trumpCardPlayer, string[] cards);
         void GameEnd(int team0Score, int team1Score);
-        void SessionEnd(int team0Score, int team1Score);
+        void SessionEnd(int sessionId, int team0Score, int team1Score);
         void Shuffle(int playerId);
         void Cut(int playerId);
         void Deal(int playerId);
@@ -39,10 +39,10 @@ namespace SuecaMessages
         void Decision(string card, string rank, string suit, string followingInfo);
         void MoveExpectations(int playerId, string desirability, string desirabilityForOther, string successProbability, string failureProbability, string additionalInfo);
 
-        void ForwardSessionStart(int numGames, int numRobots, int playerId);
+        void ForwardSessionStart(int sessionId, int numGames, int numRobots, int playerId);
         void ForwardGameStart(int gameId, int playerId, int teamId, string trumpCard, int trumpCardPlayer, string[] cards);
         void ForwardGameEnd(int team0Score, int team1Score);
-        void ForwardSessionEnd(int team0Score, int team1Score);
+        void ForwardSessionEnd(int sessionId, int team0Score, int team1Score);
         void ForwardShuffle(int playerId);
         void ForwardCut(int playerId);
         void ForwardDeal(int playerId);
